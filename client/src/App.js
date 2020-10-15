504e312d1e6acb64b5fd4e3b8d85986d60944143
AddeFreak <andreas.frick.toresand@hotmail.com>
2020-10-15 11:24:59 +0200

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,30 @@ import Checkout from './components/checkout/Checkout'
 import CheckoutState from './context/checkout/CheckoutState'
 import AlertState from './context/alert/AlertState'
 import Alert from './components/menu/Alert'
-//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <AlertState>
       <CheckoutState>
-        <>
-          <Alert />
-          <Checkout />
-          <FoodList />
-        </>
+        <Router>
+          <>
+            <Alert />
+            <Checkout />
+            <FoodList />
+            <Switch>
+              {/* <Route exact path='/' component={Home} /> */}
+              {/* <Route exact path='/login' component={Login} /> */}
+              {/* <Route exact path='/soups' component={Soups} /> */}
+              {/* <Route exact path='/protein' component={Protein} />
+              <Route exact path='/bread' component={Bread} />
+              <Route exact path='/spreads' component={Spreads} />
+              <Route exact path='/beverages' component={Beverages} /> */}
+              <Route exact path='/checkout' component={Checkout} />
+              {/* <Route component={NotFound} /> */}
+            </Switch>
+          </>
+        </Router>
       </CheckoutState>
     </AlertState>
   );
