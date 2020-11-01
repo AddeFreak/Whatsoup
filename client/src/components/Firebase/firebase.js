49b92c4fb36d5fac89714d18af0384e69d6e8dff
AddeFreak <andreas.frick.toresand@hotmail.com>
2020-11-01 22:39:27 +0100

    fix

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-    apiKey: "AIzaSyD7y66EHh60ExaxREgS5v9mcM587jQq4dQ",
-    authDomain: "ey-whatsoup.firebaseapp.com",
-    databaseURL: "https://ey-whatsoup.firebaseio.com",
-    projectId: "ey-whatsoup",
-    storageBucket: "ey-whatsoup.appspot.com",
-    messagingSenderId: "150063148708",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 class Firebase {
     constructor() {
